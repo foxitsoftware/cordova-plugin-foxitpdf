ea82525ea7fffbc3ef85552e678b54f9c4ae6cf5
xinqiang_song <xinqiang_song@foxitsoftware.com>
2019-05-13 18:18:13 +0800

    Add android demo desc and readmeimg.

--- demo/demoApp/www/js/index.js
+++ demo/demoApp/www/js/index.js
@@ -50,25 +50,27 @@ window.preview = function(){
     var sn = 'nt4MfRHH+05GEO/9DEJZFuh1shwOhyHfo89Vki8tNbGJSk5cUHnHnQ==';
     var key = 'ezJvjl3GtG539PsXZqXcImmEXhuiodACWf3giDcZ4Qnhp7nnD+Ya2PnVL+yd393PG9ZR8mfhQ3Ux2tdnplM1VSpe2Kms0pk4/0WEGhV9RtjvWd4XO1hUaeeY4DrFXN7uNRyNCl2G6kLGvlv59kMq56qflOI/2688le9VBLwtaQk05dkdrQ65e6yjDN2PQmV5QZcVLfi7vVlRvQnUIB6vtFrY4fCrycXaBG2/3Gw1v7CzvboDTiiheUDT7GqSfAY1Y3ITf2HTw7OC5KU1m7qVWia2V4VQkT/l1JaEAk5Fe6ZZBd3jPGpj7GXcBIXrUdEfJQj1qbEVB5OFO8peNA/s851LhBkcRN25wydMJG4Bsbdzm0Kp8gIL3DKf6W0aI6nsFyUc8EeUpkYY6HN2MoUg/OJsbqotaBI0cx6NZu038s3gEBBhqUGSHO5x7dzDMojU6jRADpfs6fqOFn2kQIk9JCq7fPyBzJBHFsRmh3eJ6JnEzMfjrZQLQ1Hbj+/74EzDqydz1AoPdSZYfXQ4HlGClYCKEpaVgpJhemYQxS43kWECQ4Kwa/99U6IxYWKoQ+Jm/DmABbcc8V086c9rHd5ClDFWk2PQ1BsGWSHFeuwW/sauDu+IPsAEukusIiP7DcN6J9rkmnPW2exgZKVpQlF3sqwIkm9XKxiFwuEdq+9eRI1e8d7C+91cFr9sc0idkkJCloLjUqfNunjeKVKU49Kyhp8kc1fdI06iMrg0kMzorewqnkjwqLEigFAxEOO1LyDXDTTuy4DFPX+ggVaqnmAicA8bqMlndMqWARpED8VOnjTNlNsZXf5xgJiYz1WfcVbIvTqKEITo1zxivuTCpfM+h3Sra0kH0LJmKiTsvnHsPKHDbgswwBZaUByO2cmRxw5bj12ncfLBV41NCalG84ocgck1LhIshHde0n9zp6ufuctCtkfnpM5iP5qTsTZpiyyKQ5YbVNN15DEN50JKh9KZXGXSJoLTdLfRIgalXttN0g+w21m8JegLZl483vl3hm2L7q9HUnJ/gP9MilIbK2JnV49rMte5MH7qBxFmqwOH3RfgARsjsfju8MLDJg3NYAzfUjPyLyJpVX7VvqLe5RO99jVPXnKCRPXzCq4wDwVYEmRSHjc7sgUyS2UOUf/aFNZKG6284yrNCPqaPmUjvtbmEXJyPW5QMUN+mJv2/uBm3FUUqiJcbUeM+fVqM8c7aazA2zw0f43JNBMSgn8RbEbph4L86polwCK8YVamUuWUsw==';
     window.FoxitPdf.initialize(sn,key);
-    
-    // var filePath = "/storage/emulated/0/Download/getting_started_android";
-    //var filePath = "/storage/emulated/0/Sample.pdf";
+
+
+
+    //iOS
     var filePath = cordova.file.documentsDirectory + 'getting_started_ios.pdf';
-    
-    var pdfviewoptions = {
-      'filePath': filePath,
-      'filePathSaveTo': cordova.file.documentsDirectory + 'getting_stared_ios_2.pdf',
-    };
-    window.FoxitPdf.openDocument(filePath,null);
-    
+    window.FoxitPdf.openDocument(filePath, null);
+
     window.FoxitPdf.addEventListener('onDocWillSave',function(data){
                                      console.log('onDocWillSave callback ',data);
                                      console.log('onDocWillSave callback info',data.info);
                                      //console.log('onDocWillSave callback info error',data.error);
-                                     
+
                                      console.log('66666');
                                      // window.FoxitPdf.importFromFDF(cordova.file.documentsDirectory + 'Annot_all_2.fdf',0x0002,[[0,1]]);
                                      window.FoxitPdf.exportToFDF(cordova.file.documentsDirectory +'2.xfdf',0x0002,1,[[0,1]]);
                                      console.log('555555555555555555');
                                      });
+
+    //Android
+//    var filePath = cordova.file.externalRootDirectory + 'FoxitSDK/Sample.pdf';// var filePath = "/storage/emulated/0/Sample.pdf";
+//    var password = '';
+//    window.FoxitPdf.openDocument(filePath, password);
+
 };
