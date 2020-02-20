dd1be3df075d6a332af94cecf26f6a28b2b1d2a5
xinqiang_song <xinqiang_song@foxitsoftware.com>
2020-02-20 10:24:18 +0800

    Add copyright

--- src/android/com/foxit/cordova/plugin/FoxitPdf.java
+++ src/android/com/foxit/cordova/plugin/FoxitPdf.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (C) 2003-2020, Foxit Software Inc..
+ * All Rights Reserved.
+ * <p>
+ * http://www.foxitsoftware.com
+ * <p>
+ * The following code is copyrighted and is the proprietary of Foxit Software Inc.. It is not allowed to
+ * distribute any parts of Foxit PDF SDK to third party or public without permission unless an agreement
+ * is signed between Foxit Software Inc. and customers to explicitly grant customers permissions.
+ * Review legal.txt for additional license and legal information.
+ */
 package com.foxit.cordova.plugin;
 
 import android.app.Activity;
